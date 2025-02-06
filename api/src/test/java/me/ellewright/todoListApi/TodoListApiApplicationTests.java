92919420ef513adf86a113d1b94fe2a01d97ee46
Eleanor Wright <elliewright055@gmail.com>
2025-02-06 17:06:33 -0600

    Initialize Spring Data MongoDB API

---
+++ api/src/test/java/me/ellewright/todoListApi/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ellewright.todoListApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
