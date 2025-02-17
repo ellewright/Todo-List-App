03f92e1b4dd40f67edd82374d06b89d80a5c78a1
Eleanor Wright <elliewright055@gmail.com>
2025-02-17 15:12:33 -0600

    Create FallbackController

---
+++ api/src/main/java/me/ellewright/todoListApi/controllers/FallbackController.java
@@ -0,0 +1,12 @@
+package me.ellewright.todolistapi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FallbackController {
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/index.html";
+    }
+}
